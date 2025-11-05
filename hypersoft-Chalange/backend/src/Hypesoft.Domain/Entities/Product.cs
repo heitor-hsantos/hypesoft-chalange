1592d1f16780d5bf2b17e3b42b1b3447c920ed5f
heitor-hsantos <heitor.santos118@gmail.com>
2025-11-05 13:27:33 -0300

    feat: Add HealthCheck endpoint and update project configuration

--- hypersoft-Chalange/backend/src/Hypesoft.Domain/Entities/Product.cs
+++ hypersoft-Chalange/backend/src/Hypesoft.Domain/Entities/Product.cs
@@ -8,23 +8,22 @@ namespace Hypesoft.Domain.Entities
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string Id { get; set; }
+        public required string Id { get; set; }
         
         [BsonElement("name")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
         
         [BsonElement("price")]
-        public decimal Price { get; set; }
+        public required decimal Price { get; set; }
         
         [BsonElement("description")]
         public string Description { get; set; }
         
         [BsonElement("category")]
-        public string Category { get; set; }
+        public required string Category { get; set; }
         
         [BsonElement("stockQuantity")]
-        public int StockQuantity { get; set; }
-        
+       public required int StockQuantity { get; set; }
         
         
     }
